f968ef39154730531a97db0dc9737c4db0bed5b9
mahyuddin <mahyuddin@gmail.com>
2016-05-07 06:52:34 +0800

    modify newRemoteFace
    listen as localhost and initiate connection to remote by sending empty
    packet (reverted from commit e759de03974e8765d0dfa7c3af553cd225f6c51c)

--- face.go
+++ face.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"net"
 	"time"
 
 	"github.com/go-ndn/log"
@@ -39,7 +38,7 @@ func newFace(network, address string, recv chan<- *ndn.Interest) (f *face, err e
 }
 
 func newRemoteFace(network, address string, recv chan<- *ndn.Interest) (f *face, err error) {
-	ln, err := packet.Listen(network, "0.0.0.0:6364")
+	ln, err := packet.Listen(network, address)
 	if err != nil {
 		return
 	}
@@ -47,17 +46,6 @@ func newRemoteFace(network, address string, recv chan<- *ndn.Interest) (f *face,
 	conn, err := ln.Accept()
 	if err != nil {
 		return
-	}
-	
-	raddr, err := net.ResolveUDPAddr("udp",address)
-	if err != nil {
-		return
-	}
-	
-	// send empty packet to initiate connection with multicast group
-	pconn := conn.(net.PacketConn)
-	if _,err := pconn.WriteTo([]byte(""),raddr); err != nil {
-		return nil, err
 	}
 	
 	f = &face{
